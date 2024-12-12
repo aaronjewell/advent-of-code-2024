8c2e34d1027de20840f9c1c9f6ccbeb187a88fed
Aaron Jewell <me@aaronjewell.dev>
2024-12-11 22:24:17 -0500

    Adds day 8 part 1.

---
+++ 08/part1.ts
@@ -0,0 +1,71 @@
+async function getAntennaMap(): Promise<string[][]> {
+  const file = Bun.file(import.meta.dir + '/input.txt');
+  const raw = await file.text();
+  return raw.trim().split('\n').map((line) => line.split(''));
+}
+
+function getAntennaLocationsByFreq(map: string[][]): Map<string, [number, number][]> {
+  const antennaLocations = new Map<string, [number, number][]>();
+  for (let row = 0; row < map.length; row++) {
+    for (let col = 0; col < map[row].length; col++) {
+      const cell = map[row][col];
+      if (cell === '.') {
+        continue;
+      }
+      if (!antennaLocations.has(cell)) {
+        antennaLocations.set(cell, []);
+      }
+      antennaLocations.get(cell)!.push([col, row]);
+    }
+  }
+  return antennaLocations;
+}
+
+function inBounds(map: string[][], point: [number, number]) {
+  const [row, col] = point;
+  return row >= 0 && row < map.length && col >= 0 && col < map[row].length;
+}
+
+function addVectors(v1: [number, number], v2: [number, number]): [number, number] {
+  return [v1[0] + v2[0], v1[1] + v2[1]];
+}
+
+function subtractVectors(v1: [number, number], v2: [number, number]): [number, number] {
+  return [v1[0] - v2[0], v1[1] - v2[1]];
+}
+
+function serializeVector(v: [number, number]): string {
+  return `${v[0]},${v[1]}`;
+}
+
+function uniqueAntinodesInBounds(map: string[][]): number {
+  const antennaLocations = getAntennaLocationsByFreq(map);
+  const antinodes = new Set<string>();
+
+  for (const [_, locations] of antennaLocations) {
+    for (let i = 0; i < locations.length; i++) {
+      for (let j = i + 1; j < locations.length; j++) {
+        const p1 = locations[i];
+        const p2 = locations[j];
+
+        const displacement = subtractVectors(p2, p1);
+
+        const antiparallelToP1 = subtractVectors(p1, displacement);
+        if (inBounds(map, antiparallelToP1)) {
+          antinodes.add(serializeVector(antiparallelToP1));
+        }
+
+        const antiparallelToP2 = addVectors(p2, displacement);
+        if (inBounds(map, antiparallelToP2)) {
+          antinodes.add(serializeVector(antiparallelToP2));
+        }
+      }
+    }
+  }
+
+  return antinodes.size;
+}
+
+console.log(uniqueAntinodesInBounds(await getAntennaMap()));
+
+export { }
